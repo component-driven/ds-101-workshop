45a87c07fa6884c0cffbb779527a7bdfacdd8832
Andrey Okonetchnikov <andrey@okonet.ru>
2020-04-11 15:53:52 +0200

    Use mdx-deck-theme

--- slides/theme/layouts/Primary.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { Invert } from 'mdx-deck';
-
-export default props => <Invert bg="primary" {...props} />;
--- slides/theme/layouts/Secondary.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Color } from "mdx-deck";
-
-export default (props) => (
-	<Color bg="secondary" color="background" {...props} />
-);
