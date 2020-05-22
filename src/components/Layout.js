2c8a9eb8332d7a75d403e86c442c57ef5c8e8e25
Andrey Okonetchnikov <andrey@okonet.ru>
2020-05-22 15:13:40 +0200

    Remove Google fonts loading and replace with MonoLisa

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,20 +1,34 @@
 import React from "react";
-import { Box, Flex, Grid } from "theme-ui";
+import { Box, Flex, Grid, ThemeProvider } from "theme-ui";
+import Helmet from "react-helmet";
 import { Link } from "gatsby";
+import theme from "@component-driven/mdx-deck-theme/dist/theme";
 
 export default (props) => {
   return (
-    <Flex sx={{ flexDirection: "column" }}>
-      <Box as="header">
-        <Grid as="nav" gap={4} sx={{ gridAutoFlow: "column" }}>
-          <Link to={"/"}>Home</Link>
-          <Link to={"/making-a-toast"}>Making a toast</Link>
-          <Link to={"/naming"}>Naming</Link>
-          <Link to={"/ui-inventory"}>UI Inventory</Link>
-        </Grid>
-      </Box>
-      <Box as="main">{props.children}</Box>
-      <Box as="footer">Footer</Box>
-    </Flex>
+    <>
+      <Helmet>
+        <link
+          type="text/css"
+          href="https://www.monolisa.dev/static/fonts.css"
+        />
+      </Helmet>
+      <Flex sx={{ flexDirection: "column", p: 3, minHeight: "100vh" }}>
+        <Box as="header">
+          <Grid as="nav" gap={4} sx={{ gridAutoFlow: "column" }}>
+            <Link to={"/"}>Home</Link>
+            <Link to={"/making-a-toast"}>Making a toast</Link>
+            <Link to={"/naming"}>Naming</Link>
+            <Link to={"/ui-inventory"}>UI Inventory</Link>
+          </Grid>
+        </Box>
+        <Box as="main" sx={{ flexGrow: 1 }}>
+          {props.children}
+        </Box>
+        <Box as="footer" sx={{ flexGrow: 0 }}>
+          Footer
+        </Box>
+      </Flex>
+    </>
   );
 };
