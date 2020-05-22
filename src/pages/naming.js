b66b0cfb4c92021a37d1ed0687d0b07985d781df
Varya Stepanova <mail@varya.me>
2020-05-22 13:44:52 +0300

    Show Naming review on slides

--- src/pages/naming.js
+++ src/pages/naming.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Grid, Image } from "theme-ui";
 import { graphql, useStaticQuery, Link } from "gatsby";
-import Layout from "../../components/Layout";
-import SEO from "../../components/seo";
+import Layout from "../components/Layout";
+import SEO from "../components/seo";
 
 export default () => {
   const pages = useStaticQuery(graphql`
